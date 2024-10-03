c10071d2ff0782373cbef192621d86b232abf4fe
Shehryar1021 <kiranimtiaz1985@gmail.com>
2024-10-03 15:56:05 +0500

    initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+
+tree1=23
+tree2=31
+tree3=29
+tree4=19
+tree5=25
+
+#FINDING THE TOTAL OF TREES
+sum =tree1+2+3+4+5
+print("the sum of all the five trees is: " , sum)
+ 
+ #finding the average of trees
+ average = sum\5
+ print("the average  of all the  trees is:" , average)
